ad793c917ca9a6ab75acdf4fff1d573cf433a09a
diamond <topdeveloper97@gmail.com>
2023-05-03 15:08:43 +0300

    Add: nodemon

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ app.get("/", (req: Request, res: Response) => {
   res.json({ data: "Healthy" });
 });
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on PORT ${PORT}`);
