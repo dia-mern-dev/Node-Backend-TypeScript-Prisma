b288247e427856b576c0c96d54697596d014b91e
diamond <topdeveloper97@gmail.com>
2023-05-03 14:52:34 +0300

    Update: gitignore

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Healthy");
+  res.json({ data: "Healthy" });
 });
 
 const PORT = process.env.PORT || 8000;
