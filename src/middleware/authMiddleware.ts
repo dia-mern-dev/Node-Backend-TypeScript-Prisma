11e7ac675f51129ec6e498a75a9d754f26066874
diamond <topdeveloper97@gmail.com>
2023-05-05 02:31:08 +0300

    Update: authmiddleware

--- src/middleware/authMiddleware.ts
+++ src/middleware/authMiddleware.ts
@@ -1,7 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 import jwt, { JwtPayload } from "jsonwebtoken";
 
-import prisma from "../services/prisma";
 import { StatusCodes } from "http-status-codes";
 
 declare module "express-serve-static-core" {
@@ -18,19 +17,14 @@ export const authMiddleware = async (req: Request, res: Response, next: NextFunc
     }
     const payload = jwt.verify(bearerToken?.split(" ")[1] ?? "", process.env.ACCESS_SECRET_KEY ?? "") as jwt.JwtPayload;
 
-    const user = await prisma.user.findUnique({ where: { id: parseInt(payload.id) } });
-    if (!user) {
-      return res.status(StatusCodes.UNAUTHORIZED).json({ message: "Unregistered account" });
-    }
-
     req.payload = payload;
   } catch (error) {
     if (error?.name === "TokenExpiredError") {
-      return res.status(StatusCodes.EXPECTATION_FAILED).json({ message: "Token Expired" });
+      return res.status(StatusCodes.EXPECTATION_FAILED).json({ message: "Token expired" });
     }
 
     if (error?.name === "JsonWebTokenError") {
-      return res.status(StatusCodes.EXPECTATION_FAILED).json({ message: "InValid Token" });
+      return res.status(StatusCodes.BAD_REQUEST).json({ message: "InValid token" });
     }
     return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: error });
   }
